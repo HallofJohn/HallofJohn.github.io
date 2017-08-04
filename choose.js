b2fe051ce1fab845874bccd42c82c26bd9b9712e
returnexitsuccess <returnexitsuccess@gmail.com>
2017-08-03 20:26:21 -0700

    aesthic

--- choose.js
+++ choose.js
@@ -1,7 +1,11 @@
-function pickQuote() {
-  var len = quotes.length;
-  var choice = Math.floor(Math.random() * len);
-  var quote = quotes[choice];
-  $("#quote").text('"' + quote.quote + '"');
-  $("#author").text("-" + quote.author);
+window.onload = function() {
+  var a = document.getElementById("button");
+  a.onclick = function() {
+    var len = quotes.length;
+    var choice = Math.floor(Math.random() * len);
+    var quote = quotes[choice];
+    $("#quote").text('"' + quote.quote + '"');
+    $("#author").text("-" + quote.author);
+    return false;
+  }
 }
