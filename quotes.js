8a02d14b9fdb1eeaf04abd02afbcaf02d46ca14a
returnexitsuccess <returnexitsuccess@gmail.com>
2017-08-03 18:29:25 -0700

    Added more quotes and added external stylesheet

--- quotes.js
+++ quotes.js
@@ -4,4 +4,52 @@ var quotes = [{
 }, {
   "quote": "Babies are magnetic.",
   "author": "John Dewhirst"
+}, {
+  "quote": "Noah, I'm going to assault his car!",
+  "author": "John Dewhirst"
+}, {
+  "quote": "Noah, you don't understand because you don't understand.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "They're not dented, that's a guys face.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "Michael! I have a dirty penny.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "No, Michael, I'm really good conductive.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "My ass has a lot of stuff.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "I feel broharassed.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "Broassed sounds much worse.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "Sphinxter.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "What spins and screws? A corkscrew. What spins and screws and leaves you sore in the morning? John with a corkscrew.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "Ya, .08 is the limit which is 8%.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "No Eric, you can't drink blood.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "My crutch, my rules.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "It's only sodomy if it's my penis.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "Almost ridiculously how sober I am.",
+  "author": "John Dewhirst"
+}, {
+  "quote": "Shut the up, Michael.",
+  "author": "John Dewhirst"
 }];
